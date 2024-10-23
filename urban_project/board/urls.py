3deeb928cb719195f4385e131a23e17405c91071
Илья <ilialuchko@gmail.com>
2024-10-23 23:15:19 +0500

    Resolved merge conflicts

--- urban_project/board/urls.py
+++ urban_project/board/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path  # Импорт функции, которая используется для определения шаблонов URL
-from . import views  # Импорт модуля из текущего пакета для ссылки на функции представлений
+from django.urls import path
+from . import views
 
-app_name = 'board'  # Задаем пространство имен для приложения (URL-имена будут уникальными для разных приложений).
-
+app_name = 'board'
+"""
+Задаем пространство имен для приложения (URL-имена будут уникальными для разных приложений).
+Здесь находится список шаблонов URL, которые сопоставлены URL с представлениями. 
+"""
 urlpatterns = [
-    # Список шаблонов URL, которые сопоставляют URL с представлениями.
     path('', views.advertisement_list, name='advertisement_list'),
     path('advertisement/<int:pk>/', views.advertisement_detail, name='advertisement_detail'),
     path('add/', views.add_advertisement, name='add_advertisement'),
     path('advertisement/<int:pk>/edit/', views.edit_advertisement, name='edit_advertisement'),
+    path('advertisement/<int:pk>/delete/', views.delete_advertisement, name='delete_advertisement'),
 ]
